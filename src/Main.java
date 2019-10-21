c6bdfd8f67c5a97c006438d8b23cc06e0a32f1a2
fenglin098 <feng.lin098@gmail.com>
2019-10-21 16:16:22 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String [] args){
+        //Printing out hello world to the console
         System.out.println("Hello World");
+        //Declaration of a variable
+        String name;
+        //Initialization of a variable
+        name = "Feng";
+        //declaration and initialization of a variable in the same line
+        String newName = "Brad";
+        //reinitializing a variable
+        name = "Sam";
+        System.out.println(name);
     }
 }
